4cb87b99528b38133778a129277eec1698001e4e
Andreas Fink <andreas.fink@cscs.ch>
2019-02-21 11:01:01 +0100

    *add* added a build_all script

---
+++ build_all.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -e
+set -x
+
+for i in {1..6} ; do
+    docker images | grep -q stage0${i} || ( docker build -t=finkandreas/utopia:stage0${i} -f Dockerfile.stage0${i} . && docker push finkandreas/utopia:stage0${i} )
+done
+
